6fcc1ffddbd145dfd307926248ac34f8a3acb333
Jim Tittsler <jimt@oer.me>
2026-07-04 08:57:41 +0000

    MW1.35 hooks
    use hook for setting copyright in header
    use hook for replacing copyright in footer

--- LicenseByCategory.php
+++ LicenseByCategory.php
@@ -1,56 +1,19 @@
 <?php
 # ex: tabstop=8 shiftwidth=8 noexpandtab
 /**
-* @package MediaWiki
-* @subpackage LicenseByCategory
-* @author Jim Tittsler <jim@OERfoundation.org>
-* @licence GPL2
-*/
+ * @package MediaWiki
+ * @subpackage LicenseByCategory
+ * @author Jim Tittsler <jim@OERfoundation.org>
+ * @licence GPL2
+ */
 
 if( !defined( 'MEDIAWIKI' ) ) {
 	die( "This file is an extension to the MediaWiki software and cannot be used standalone.\n" );
 }
 
-$wgExtensionCredits['parserhook'][] = array(
-	'path'           => __FILE__,
-	'name'           => 'LicenseByCategory',
-	'version'        => '1.1.3',
-	'url'            => 'http://WikiEducator.org/Extension:LicenseByCategory',
-	'author'         => '[http://WikiEducator.org/User:JimTittsler Jim Tittsler]',
-	'description'    => 'Change header(link) and footer license based on category',
-	'license'	 => 'GPL-2.0'
-);
+class LicenseByCategoryHooks {
 
-$wgHooks['OutputPageMakeCategoryLinks'][] = 'weCategoryLinks';
-$wgHooks['SkinTemplateOutputPageBeforeExec'][] = 'weLicenseByCategory';
-
-function weCategoryLinks( &$out, $categories, &$links ) {
-	global $weLicense;
-	$weLicense = 'CC-BY-SA';
-	if ( array_key_exists( 'CC0', $categories ) || array_key_exists( 'CC-0', $categories ) ) {
-		$weLicense = 'CC0';
-	} elseif ( array_key_exists( 'Public_Domain', $categories ) ) {
-		$weLicense = 'PD';
-	} else {
-		foreach ( $categories as $cat => $v ) {
-			if ( preg_match( "/^cc-by((_pages)|(-[0-9.]+))?$/i",
-					$cat ) ) {
-				$weLicense = 'CC-BY';
-				break;
-			}
-		}
-	}
-	return true;
-}
-
-function weLicenseByCategory ( &$templateEngine, &$tpl ) {
-	global $weLicense;
-	$weCopyrights = array(
-		'CC-BY' => 'Content is available under the <a rel="license" href="http://creativecommons.org/licenses/by/3.0" class="external" title="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution License</a>.',
-		'CC0' => 'Content is available under a <a rel="license" href="http://creativecommons.org/publicdomain/zero/1.0/" class="external" title="http://creativecommons.org/publicdomain/zero/1.0/">CC0 Public Domain Dedication</a>.',
-		'PD' => 'Content has been released under a <a rel="license" href="http://WikiEducator.org/WikiEducator:Public_Domain" class="external" title="http://WikiEducator.org/WikiEducator.org/Public_Domain">Public Domain dedication</a>.'
-	);
-	$weCopyrightIcons = array(
+	private const LICENSE_ICONS = array(
 		'CC-BY' => array(
 			'url' => 'http://creativecommons.org/licenses/by/3.0/',
 			'src' => '/extensions/LicenseByCategory/icons/cc-by.png',
@@ -68,24 +31,59 @@ function weLicenseByCategory ( &$templateEngine, &$tpl ) {
 		),
 	);
 
-	if ( isset( $weLicense ) && array_key_exists( $weLicense, $weCopyrightIcons ) ) {
-		$tpl->set( 'copyright', $weCopyrights[$weLicense] );
+	private const LICENSE_TEXT = array(
+		'CC-BY' => 'Content is available under the <a rel="license" href="http://creativecommons.org/licenses/by/3.0" class="external" title="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution License</a>.',
+		'CC0' => 'Content is available under a <a rel="license" href="http://creativecommons.org/publicdomain/zero/1.0/" class="external" title="http://creativecommons.org/publicdomain/zero/1.0/">CC0 Public Domain Dedication</a>.',
+		'PD' => 'Content has been released under a <a rel="license" href="http://WikiEducator.org/WikiEducator:Public_Domain" class="external" title="http://WikiEducator.org/WikiEducator.org/Public_Domain">Public Domain dedication</a>.',
+	);
 
-		# replace both the old and new forms of the copyright icon
-		$i = $weCopyrightIcons[$weLicense];
-		$tpl->set( 'copyrightico',
-		       "<a href=\"{$i['url']}\"><img src=\"{$i['src']}\" alt=\"{$i['alt']}\" /></a>" );
-		$fi = $tpl->data['footericons'];
-		$fi['copyright']['copyright'] = $weCopyrightIcons[$weLicense];
-		$tpl->setRef( 'footericons', $fi );
-
-		# replace the old copyright link from the head
-		if ( isset( $tpl->data['headelement'] ) ) {
-			$he = $tpl->data['headelement'];
-			$he = preg_replace( '/(<link rel="copyright" href=")[^"]+/',
-				'$1'.$weCopyrightIcons[$weLicense]['url'], $he );
-			$tpl->setRef( 'headelement', $he );
+	public static function onOutputPageMakeCategoryLinks( $out, $categories, &$links ) {
+		$weLicense = 'CC-BY-SA';
+		if ( array_key_exists( 'CC0', $categories ) || array_key_exists( 'CC-0', $categories ) ) {
+			$weLicense = 'CC0';
+		} elseif ( array_key_exists( 'Public_Domain', $categories ) ) {
+			$weLicense = 'PD';
+		} else {
+			foreach ( $categories as $cat => $type ) {
+				if ( preg_match( "/^cc-by((_pages)|(-[0-9.]+))?$/i",
+						$cat ) ) {
+					$weLicense = 'CC-BY';
+					break;
+				}
+			}
 		}
+
+		if ( !array_key_exists( $weLicense, self::LICENSE_ICONS ) ) {
+			return true;
+		}
+
+		# Remember which license applies to this output, for onSkinCopyrightFooter
+		$out->setProperty( 'weLicense', $weLicense );
+
+		$icon = self::LICENSE_ICONS[$weLicense];
+
+		# Modern replacement for rewriting <link rel="copyright"> in the page head
+		$out->setCopyrightUrl( $icon['url'] );
+
+		# There is no hook for the footer copyright icon; $wgFooterIcons is read
+		# fresh from config each request, so updating it here is picked up when
+		# the skin builds the footer.
+		global $wgFooterIcons;
+		$wgFooterIcons['copyright']['copyright'] = array(
+			'url' => $icon['url'],
+			'src' => $icon['src'],
+			'alt' => $icon['alt'],
+		);
+
+		return true;
 	}
-	return true;
+
+	public static function onSkinCopyrightFooter( $title, $type, &$msg, &$link ) {
+		$weLicense = RequestContext::getMain()->getOutput()->getProperty( 'weLicense' );
+		if ( $weLicense !== null && array_key_exists( $weLicense, self::LICENSE_TEXT ) ) {
+			$msg = 'licensebycategory-copyright';
+			$link = self::LICENSE_TEXT[$weLicense];
+		}
+		return true;
+	}
 }
